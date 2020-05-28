a4f6de90211b09420db9b9e837b07e491ff34295
gaponova <dzensmart@gmail.com>
2020-05-28 16:43:12 +0300

    Начали секцию price

--- js/scroll-menu.js
+++ js/scroll-menu.js
@@ -2,7 +2,7 @@ window.onscroll = () => {
   const headerbar = document.querySelector('.header-bar');
   const Y = window.scrollY;
 
-  if (Y > 200) {
+  if (Y > 100) {
     headerbar.classList.add('bar-fixed');
   }else if (Y < 100) {
     headerbar.classList.remove('bar-fixed');
